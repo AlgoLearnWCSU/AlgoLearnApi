d2b3344430b72dc3d67f7f15a7851f0825b3eb8d
Ajetski <45019515+Ajetski@users.noreply.github.com>
2020-10-24 13:42:40 -0400

    #4 init project

---
+++ src/main/kotlin/edu/wcsu/cs360/algolearn/Server.kt
@@ -0,0 +1,12 @@
+package edu.wcsu.cs360.algolearn
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Server
+
+fun main(args: Array<String>) {
+    runApplication<Server>(*args)
+}
+
---
+++ src/test/kotlin/edu/wcsu/cs360/algolearn/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package edu.wcsu.cs360.algolearn
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
