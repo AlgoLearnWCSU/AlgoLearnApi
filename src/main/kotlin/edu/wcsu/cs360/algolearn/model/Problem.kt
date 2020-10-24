c781fb090df75352dd5dc14f7184264896c0d28a
Ajetski <45019515+Ajetski@users.noreply.github.com>
2020-10-24 14:02:47 -0400

    #5 init problem.kt

---
+++ src/main/kotlin/edu/wcsu/cs360/algolearn/model/Problem.kt
@@ -0,0 +1,25 @@
+package edu.wcsu.cs360.algolearn.model
+
+import org.springframework.data.repository.CrudRepository
+import javax.persistence.*
+
+
+@Entity // This tells Hibernate to make a table out of this class
+@Table(name = "problem", schema = "public") // change table name to avoid keyword conflict
+class Problem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    var id: Int? = null
+
+    @Column(nullable = false)
+    var description: String? = null
+
+    @Column(nullable = false)
+    var isReviewed: Boolean? = null
+
+    @Column(nullable = false)
+    val poster: String? = null
+
+}
+
+interface ProblemRepository : CrudRepository<Problem?, Int?>
