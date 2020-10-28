8a5305db2fc2a44a10cb39998790cf2499f882a2
Caleb Garrick <caleb.garrick@gmail.com>
2020-10-28 17:43:15 -0400

    #5 init solution.kt

---
+++ src/main/kotlin/edu/wcsu/cs360/algolearn/model/Solution.kt
@@ -0,0 +1,33 @@
+package edu.wcsu.cs360.algolearn.model
+
+import org.springframework.data.repository.CrudRepository
+import javax.persistence.*
+
+
+@Entity // This tells Hibernate to make a table out of this class
+@Table(name = "solution", schema = "public") // change table name to avoid keyword conflict
+class Solution {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    var id: Int? = null
+
+    @Column(nullable = false)
+    var solver: String? = null
+
+    @Column(nullable = false)
+    var problem: Int? = null
+
+    @Column(nullable = false)
+    @Lob
+    var code: String? = null
+
+    @Column(nullable = false)
+    var passedTests: Int? = null
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIME)
+    var compTime: java.util.Date? = null
+
+}
+
+interface SolutionRepository : CrudRepository<Solution?, Int?>
