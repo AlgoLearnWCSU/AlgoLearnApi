a98b88abb1def3fb8f6fb2b112dc65867e6dc255
Caleb Garrick <caleb.garrick@gmail.com>
2020-10-28 17:44:10 -0400

    #12 init test case controller

---
+++ src/main/kotlin/edu/wcsu/cs360/algolearn/controller/TestCaseController.kt
@@ -0,0 +1,79 @@
+package edu.wcsu.cs360.algolearn.controller
+
+import edu.wcsu.cs360.algolearn.model.TestCase
+import edu.wcsu.cs360.algolearn.model.TestCaseRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping(path = ["/testcase"])
+class TestCaseController {
+    @Autowired
+    private val testcaseRepository: TestCaseRepository? = null
+
+    @GetMapping
+    fun getDemTestCases(): List<TestCase?> {
+        println("test")
+        return testcaseRepository!!.findAll().toList()
+    }
+
+    @GetMapping(path = ["/{tcid}"])
+    fun getDemTestCasesById(@PathVariable tcid: Int?): Any {
+        val data = testcaseRepository!!.findById(tcid!!)
+        if (data.isEmpty)
+            return ResponseEntity<Any>(HttpStatus.BAD_REQUEST)
+        return data.get()
+    }
+
+    @PostMapping // Map ONLY POST Requests
+    fun addNewTestCase(@RequestBody testcase: TestCase): Any {
+        if (testcase.id != null &&
+                testcaseRepository!!.findById(testcase.id!!).isPresent)
+            return ResponseEntity<Any>(HttpStatus.CONFLICT)
+        testcaseRepository?.save(testcase)
+        return testcase
+    }
+
+    @PutMapping(path = ["/{tcid}"])
+    fun replaceTestCase(@PathVariable tcid: Int, @RequestBody testcase: TestCase): Any {
+        if (testcaseRepository!!.findById(tcid).isEmpty)
+            return ResponseEntity<Any>(HttpStatus.NOT_FOUND)
+        testcaseRepository.save(testcase)
+        return testcase
+    }
+
+    @PatchMapping(path = ["/{tcid}"])
+    fun modifyTestCase(@PathVariable tcid: Int, @RequestBody testcase: TestCase): Any {
+        val oldTestCase = testcaseRepository!!.findById(tcid)
+        if (oldTestCase.isEmpty)
+            return ResponseEntity<Any>(HttpStatus.NOT_FOUND)
+
+        if(tcid != testcase.id)
+            testcaseRepository.deleteById(tcid)
+
+        if(testcase.id == null)
+            testcase.id = tcid
+        if(testcase.problem == null)
+            testcase.problem = oldTestCase.get().problem
+        if(testcase.public == null)
+            testcase.public = oldTestCase.get().public
+        if(testcase.sampleInput == null)
+            testcase.sampleInput = oldTestCase.get().sampleInput
+        if(testcase.sampleOutput == null)
+            testcase.sampleOutput = oldTestCase.get().sampleOutput
+
+        testcaseRepository.save(testcase)
+
+        return testcase
+    }
+
+    @DeleteMapping(path = ["/{tcid}"])
+    fun deleteTestCaseById(@PathVariable tcid: Int): ResponseEntity<Any> {
+        if (testcaseRepository!!.findById(tcid).isEmpty)
+            return ResponseEntity(HttpStatus.BAD_REQUEST)
+        testcaseRepository.deleteById(tcid)
+        return ResponseEntity(HttpStatus.OK)
+    }
+}
