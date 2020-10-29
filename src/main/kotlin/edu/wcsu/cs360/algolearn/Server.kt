d0e8cb8d8394ff9fceb2da61d58d04f91a4d4f8e
Ajetski <45019515+Ajetski@users.noreply.github.com>
2020-10-29 19:37:58 -0400

    #4 add cors

--- src/main/kotlin/edu/wcsu/cs360/algolearn/Server.kt
+++ src/main/kotlin/edu/wcsu/cs360/algolearn/Server.kt
@@ -2,6 +2,13 @@ package edu.wcsu.cs360.algolearn
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
 
 @SpringBootApplication
 class Server
@@ -10,3 +17,20 @@ fun main(args: Array<String>) {
     runApplication<Server>(*args)
 }
 
+
+@Configuration
+class CORSAdvice {
+    @Bean
+    fun corsFilter(): FilterRegistrationBean<*> {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.addAllowedOrigin("*")
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("*")
+        source.registerCorsConfiguration("/**", config)
+        val bean: FilterRegistrationBean<*> = FilterRegistrationBean<CorsFilter>(CorsFilter(source))
+        bean.order = 0
+        return bean
+    }
+}
