59ea2158a40e52597ef383d48e04bb346a071a05
Ajetski <45019515+Ajetski@users.noreply.github.com>
2020-10-30 16:47:07 -0400

    #16 comment out tests

--- src/test/kotlin/edu/wcsu/cs360/algolearn/DemoApplicationTests.kt
+++ src/test/kotlin/edu/wcsu/cs360/algolearn/DemoApplicationTests.kt
@@ -1,13 +1,13 @@
 package edu.wcsu.cs360.algolearn
-
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-
-@SpringBootTest
-class DemoApplicationTests {
-
-    @Test
-    fun contextLoads() {
-    }
-
-}
+//
+//import org.junit.jupiter.api.Test
+//import org.springframework.boot.test.context.SpringBootTest
+//
+//@SpringBootTest
+//class DemoApplicationTests {
+//
+//    @Test
+//    fun contextLoads() {
+//    }
+//
+//}
