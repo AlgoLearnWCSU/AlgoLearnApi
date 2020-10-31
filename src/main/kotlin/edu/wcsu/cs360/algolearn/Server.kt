671e38fc6a53f01cda5656ba11b9457f5b1d9550
Caleb Garrick <caleb.garrick@gmail.com>
2020-10-31 13:47:30 -0400

    #17 add tomcat webservlet to reroute http requests

--- src/main/kotlin/edu/wcsu/cs360/algolearn/Server.kt
+++ src/main/kotlin/edu/wcsu/cs360/algolearn/Server.kt
@@ -1,8 +1,16 @@
 package edu.wcsu.cs360.algolearn
 
+// generated tomcat bean following this https://www.thomasvitale.com/https-spring-boot-ssl-certificate/
+
+import org.apache.catalina.Context
+import org.apache.catalina.connector.Connector
+import org.apache.tomcat.util.descriptor.web.SecurityCollection
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
 import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.cors.CorsConfiguration
@@ -19,7 +27,7 @@ fun main(args: Array<String>) {
 
 
 @Configuration
-class CORSAdvice {
+class ServerConfig {
     @Bean
     fun corsFilter(): FilterRegistrationBean<*> {
         val source = UrlBasedCorsConfigurationSource()
@@ -33,4 +41,30 @@ class CORSAdvice {
         bean.order = 0
         return bean
     }
+
+    @Bean
+    fun servletContainer(): ServletWebServerFactory {
+        val tomcat: TomcatServletWebServerFactory = object : TomcatServletWebServerFactory() {
+            override fun postProcessContext(context: Context) {
+                val securityConstraint = SecurityConstraint()
+                securityConstraint.userConstraint = "CONFIDENTIAL"
+                val collection = SecurityCollection()
+                collection.addPattern("/*")
+                securityConstraint.addCollection(collection)
+                context.addConstraint(securityConstraint)
+            }
+        }
+        tomcat.addAdditionalTomcatConnectors(httpConnector)
+        return tomcat
+    }
+
+    private val httpConnector: Connector
+        private get() {
+            val connector = Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL)
+            connector.scheme = "http"
+            connector.port = 8080
+            connector.secure = false
+            connector.redirectPort = 8443
+            return connector
+        }
 }
