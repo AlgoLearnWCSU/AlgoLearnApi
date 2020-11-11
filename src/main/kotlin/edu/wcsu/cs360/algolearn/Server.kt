fd06da7bf6976973e139d5979b0e96515f928d58
Ajetski <45019515+Ajetski@users.noreply.github.com>
2020-11-11 12:00:26 -0500

    #16 remove code commented for devops

--- src/main/kotlin/edu/wcsu/cs360/algolearn/Server.kt
+++ src/main/kotlin/edu/wcsu/cs360/algolearn/Server.kt
@@ -1,16 +1,8 @@
 package edu.wcsu.cs360.algolearn
 
-// generated tomcat bean following this https://www.thomasvitale.com/https-spring-boot-ssl-certificate/
-
-import org.apache.catalina.Context
-import org.apache.catalina.connector.Connector
-import org.apache.tomcat.util.descriptor.web.SecurityCollection
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
 import org.springframework.boot.web.servlet.FilterRegistrationBean
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
@@ -21,14 +13,12 @@ import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.filter.CorsFilter
 
-
 @SpringBootApplication
 class Server
 
 fun main(args: Array<String>) {
     runApplication<Server>(*args)
 }
-
 
 @Configuration
 class ServerConfig {
@@ -45,32 +35,6 @@ class ServerConfig {
         bean.order = 0
         return bean
     }
-
-//    @Bean
-//    fun servletContainer(): ServletWebServerFactory {
-//        val tomcat: TomcatServletWebServerFactory = object : TomcatServletWebServerFactory() {
-//            override fun postProcessContext(context: Context) {
-//                val securityConstraint = SecurityConstraint()
-//                securityConstraint.userConstraint = "CONFIDENTIAL"
-//                val collection = SecurityCollection()
-//                collection.addPattern("/*")
-//                securityConstraint.addCollection(collection)
-//                context.addConstraint(securityConstraint)
-//            }
-//        }
-//        tomcat.addAdditionalTomcatConnectors(httpConnector)
-//        return tomcat
-//    }
-//
-//    private val httpConnector: Connector
-//        private get() {
-//            val connector = Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL)
-//            connector.scheme = "http"
-//            connector.port = 80
-//            connector.secure = false
-//            connector.redirectPort = 443
-//            return connector
-//        }
 
     @Bean
     fun bCryptPasswordEncoder(): BCryptPasswordEncoder {
