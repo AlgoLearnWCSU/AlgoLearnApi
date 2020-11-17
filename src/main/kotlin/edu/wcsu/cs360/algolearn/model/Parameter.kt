a912828b9e3c6720fb7817264e7e96ec163ef599
tyjdor <tyjdor96@gmail.com>
2020-11-16 20:21:54 -0500

    #5 Change paramId to id
    Co-Authored-By: Adam Jeniski <45019515+Ajetski@users.noreply.github.com>

--- src/main/kotlin/edu/wcsu/cs360/algolearn/model/Parameter.kt
+++ src/main/kotlin/edu/wcsu/cs360/algolearn/model/Parameter.kt
@@ -1,14 +1,13 @@
 package edu.wcsu.cs360.algolearn.model
 
 import org.springframework.data.jpa.repository.JpaRepository
-import java.io.Serializable
 import javax.persistence.*
 
 @Entity(name = "parameter") // This tells Hibernate to make a table out of this class
 class Parameter {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    var paramId: Int? = null
+    var id: Int? = null
 
     @Column(nullable = false)
     var problem: Int? = null
@@ -18,8 +17,8 @@ class Parameter {
 
     constructor()
 
-    constructor(paramId: Int?, problem: Int?, name: String?) {
-        this.paramId = paramId
+    constructor(id: Int?, problem: Int?, name: String?) {
+        this.id = id
         this.problem = problem
         this.name = name
     }
