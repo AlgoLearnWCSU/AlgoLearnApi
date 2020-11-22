a3977b41ec50a1f46e7060e54baa42aa6f218db1
Caleb Garrick <caleb.garrick@gmail.com>
2020-11-22 18:52:53 -0500

    #8 add authorization and call update functions

--- src/main/kotlin/edu/wcsu/cs360/algolearn/controller/ProblemController.kt
+++ src/main/kotlin/edu/wcsu/cs360/algolearn/controller/ProblemController.kt
@@ -1,6 +1,10 @@
 package edu.wcsu.cs360.algolearn.controller
 
 import edu.wcsu.cs360.algolearn.model.*
+import edu.wcsu.cs360.algolearn.model.AuthRepository
+import edu.wcsu.cs360.algolearn.model.Problem
+import edu.wcsu.cs360.algolearn.model.ProblemRepository
+import edu.wcsu.cs360.algolearn.model.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.ExampleMatcher
 import org.springframework.http.HttpStatus
@@ -24,6 +28,13 @@ class ProblemController {
     @Autowired
     private val commentRepository: CommentRepository? = null
 
+    @Autowired
+    private val authRepository: AuthRepository? = null
+
+    @Autowired
+    private val userRepository: UserRepository? = null
+    
+
     @GetMapping
     fun getDemProblems(): List<Problem?> {
         return problemRepository!!.findAll().toList()
@@ -38,47 +49,103 @@ class ProblemController {
     }
 
     @PostMapping // Map ONLY POST Requests
-    fun addNewProblem(@RequestBody problem: Problem): Any {
+    fun addNewProblem(@RequestBody problem: Problem,
+                      @RequestHeader("auth-token") authToken: String): Any {
+        val authSession = authRepository!!.findById(authToken)
+
+        if(problem.isReviewed == null)
+            problem.isReviewed = false
+
+         if (authSession.isEmpty ||
+                !((authSession.get().username == problem.poster && problem.isReviewed  == false) ||
+                userRepository!!.findById(authSession.get().username!!).get().isAdmin!!))
+            return ResponseEntity<Any>(HttpStatus.UNAUTHORIZED)
+
         if (problem.id != null &&
                 problemRepository!!.findById(problem.id!!).isPresent)
             return ResponseEntity<Any>(HttpStatus.CONFLICT)
         problemRepository?.save(problem)
+
         return problem
     }
 
     @PutMapping(path = ["/{id}"])
-    fun replaceProblem(@PathVariable id: Int, @RequestBody problem: Problem): Any {
+    fun replaceProblem(@PathVariable id: Int, @RequestBody problem: Problem,
+                       @RequestHeader("auth-token") authToken: String): Any {
+
+        val authSession = authRepository!!.findById(authToken)
+
+        if(problem.isReviewed == null)
+            problem.isReviewed = false
+
         if (problemRepository!!.findById(id).isEmpty)
             return ResponseEntity<Any>(HttpStatus.NOT_FOUND)
-        problemRepository.save(problem)
+
+        if (authSession.isEmpty ||
+                !((authSession.get().username == problemRepository.findById(id).get().poster && authSession.get().username == problem.poster && problem.isReviewed  == false) ||
+                        userRepository!!.findById(authSession.get().username!!).get().isAdmin!!))
+            return ResponseEntity<Any>(HttpStatus.UNAUTHORIZED)
+
+        problem.id = id
+
+        problemRepository.updateDescriptionById(id, problem.description!!)
+        problemRepository.updateNameById(id, problem.name!!)
+        problemRepository.updatePosterById(id, problem.poster!!)
+        problemRepository.updateReviewedById(id, problem.isReviewed!!)
+
         return problem
     }
 
     @PatchMapping(path = ["/{id}"])
-    fun modifyProblem(@PathVariable id: Int, @RequestBody problem: Problem): Any {
+    fun modifyProblem(@PathVariable id: Int, @RequestBody problem: Problem,
+                      @RequestHeader("auth-token") authToken: String): Any {
+
+        val authSession = authRepository!!.findById(authToken)
+
         val oldProblem = problemRepository!!.findById(id)
         if (oldProblem.isEmpty)
             return ResponseEntity<Any>(HttpStatus.NOT_FOUND)
 
-        if(id != problem.id)
-            problemRepository.deleteById(id)
+        if (authSession.isEmpty ||
+                !((authSession.get().username == oldProblem.get().poster && (problem.poster == null || authSession.get().username == problem.poster) && (problem.isReviewed  == false || problem.isReviewed  == null )) ||
+                        userRepository!!.findById(authSession.get().username!!).get().isAdmin!!))
+            return ResponseEntity<Any>(HttpStatus.UNAUTHORIZED)
 
-        if(problem.id == null)
-            problem.id = id
-        if(problem.description == null)
+        problem.id = id
+
+        if(problem.description != null)
+            problemRepository.updateDescriptionById(id, problem.description!!)
+        else
             problem.description = oldProblem.get().description
-        if(problem.isReviewed == null)
+        if(problem.isReviewed != null)
+            problemRepository.updateReviewedById(id, problem.isReviewed!!)
+        else
             problem.isReviewed = oldProblem.get().isReviewed
-
-        problemRepository.save(problem)
+        if(problem.poster != null)
+            problemRepository.updatePosterById(id, problem.poster!!)
+        else
+            problem.poster = oldProblem.get().poster
+        if(problem.name != null)
+            problemRepository.updateNameById(id, problem.name!!)
+        else
+            problem.name = oldProblem.get().name
 
         return problem
     }
 
     @DeleteMapping(path = ["/{id}"])
-    fun deleteProblemById(@PathVariable id: Int): ResponseEntity<Any> {
+    fun deleteProblemById(@PathVariable id: Int,
+                          @RequestHeader("auth-token") authToken: String): ResponseEntity<Any> {
+
+        val authSession = authRepository!!.findById(authToken)
+
         if (problemRepository!!.findById(id).isEmpty)
             return ResponseEntity(HttpStatus.BAD_REQUEST)
+
+        if (authSession.isEmpty ||
+            !((authSession.get().username == problemRepository.findById(id).get().poster ) ||
+                    userRepository!!.findById(authSession.get().username!!).get().isAdmin!!))
+        return ResponseEntity<Any>(HttpStatus.UNAUTHORIZED)
 
         // Delete TestCases
         testcaseRepository!!.findAll(
@@ -97,7 +164,7 @@ class ProblemController {
                 Example.of(Parameter(null, id, null), ExampleMatcher
                         .matchingAll()
                         .withMatcher("problem", exact()))).forEach {
-            testcaseRepository.deleteById(it.id!!)
+            parameterRepository.deleteById(it.id!!)
         }
 
         // Delete Comments
@@ -107,6 +174,7 @@ class ProblemController {
                         .withMatcher("problem", exact()))).forEach {
             commentRepository.deleteById(it.id!!)
         }
+
 
         problemRepository.deleteById(id)
         return ResponseEntity(HttpStatus.OK)
